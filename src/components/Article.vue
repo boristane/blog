0c6e827de2237dedac4e5996ea0fd8202e77c555
boristane <boris.tane@gmail.com>
2018-10-26 23:08:52 +0100

    displays articles

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="article">
-        <div>Article {{ article }} {{ articleTitle }} </div>
+        <p class="date">{{ date }}</p>
+        <div v-html="html"></div>
     </div>
 </template>
 
 <script>
+import showdown from 'showdown';
+const apiURL = 'http://127.0.0.1:3000';
+
+function loadFile(filePath) {
+  let result = null;
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
+
+let converter = new showdown.Converter();
+
 export default {
   name: 'Article',
   data () {
@@ -19,13 +36,20 @@ export default {
       article () {
           if(this.articles) return this.articles.find(article => article.title.toLowerCase() === this.articleTitle);
           else return {};
+      },
+      date () {
+         return `${(new Date(this.article.createdAt)).toDateString().slice(4, 15)}`;
+     },
+      md () {
+          return loadFile(`${apiURL}/${this.article.content}`);
+      },
+      html () {
+          return converter.makeHtml(this.md);
       }
   }
 }
 </script>
 
 <style scoped>
-.article {
-    color: red;
-}
+
 </style>
